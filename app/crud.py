e2265271ea1b4de7ddb8444be783005b67c77fe6
issei.saiki <issei.saiki@brianpad.co.jp>
2024-07-09 21:15:42 +0900

    商品閲覧でカテゴリによるフィルターをかけられるように修正

--- app/crud.py
+++ app/crud.py
@@ -2,8 +2,11 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+def get_items(db: Session, category: str | None = None, skip: int = 0, limit: int = 100):
+    query = db.query(models.Item)
+    if category:
+        query = query.filter(models.Item.category == category)
+    return query.offset(skip).limit(limit).all()
 
 def create_item(item: schemas.ItemCreate, db: Session):
     db_item = models.Item(**item.dict())
--- app/routers/customer.py
+++ app/routers/customer.py
@@ -9,7 +9,8 @@ from app.dependency import get_db
 router = APIRouter()
 
 @router.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
+def read_items(
+        category: str = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+        ):
+    items = crud.get_items(db, category=category, skip=skip, limit=limit)
     return items
-
