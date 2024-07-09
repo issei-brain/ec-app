fc6cfe0c55ce3960d559a4eaac0cc8f63c9b3412
issei.saiki <issei.saiki@brianpad.co.jp>
2024-07-09 11:12:43 +0900

    /admin/item/追加

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routers import customer, admin
 
 app = FastAPI()
+
+# app.include_router(customer.router, prefix="/customer", tags=["customer"])
+app.include_router(admin.router, prefix="/admin", tags=["admin"])
 
 @app.get("/")
 async def root():
