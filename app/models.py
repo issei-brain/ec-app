670db79b82bf2852c3f3f8ba6ed4940c114f2b99
issei.saiki <issei.saiki@brianpad.co.jp>
2024-07-11 10:05:06 +0900

    modelsにOrderクラスを追加

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -11,9 +11,24 @@ class Item(Base):
     category = Column(String, index=True)
     description = Column(String)
 
+    orders = relationship("Order", back_populates="item")
+
 class Customer(Base):
     __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, index=True)
     age = Column(Integer)
+
+    orders = relationship("Order", back_populates="customer")
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    item_id = Column(Integer, ForeignKey("items.id"))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    order_date = Column(DateTime)
+
+    customer = relationship("Item", back_populates="orders")
+    item = relationship("Order", back_populates="orders")
