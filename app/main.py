52a20a86915cb5f3fb2656f8a767603ea5923384
issei.saiki <issei.saiki@brianpad.co.jp>
2024-07-22 19:00:44 +0900

    テストを実行するとsql_app.dbができる問題を解消

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ app = FastAPI()
 app.include_router(customer.router, prefix="/customer", tags=["customer"])
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
 
-models.Base.metadata.create_all(bind=engine)
-
 @app.get("/")
 async def root():
     return {"message": "Welcome to ecomerce app"}
+
+
+if __name__ == "__main__":
+    models.Base.metadata.create_all(bind=engine)
