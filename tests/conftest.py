56b3d5297f4748869c5538d515d3cfab53786ec6
issei.saiki <issei.saiki@brianpad.co.jp>
2024-07-23 11:40:17 +0900

    test_sys_path.pyを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import sys
 import os
 
+# pytestがアプリケーションを読み込めるようにsys.pathを設定
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
+
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,2 +1,2 @@
-def test_get_items():
-    assert 1 == 1
+def test_get_items(some_data):
+    assert some_data == 42
